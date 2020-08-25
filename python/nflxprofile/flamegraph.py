1f79d1264a25a9da589f75dde2dcbbc86ed260b6
Martin Spier <spiermar@gmail.com>
2020-08-25 15:51:30 -0700

    Revert "feat: option to ignore libtype"
    This reverts commit 41df0d0087dfe58c74aeee83e01f741bfbfae420.

--- python/nflxprofile/flamegraph.py
+++ python/nflxprofile/flamegraph.py
@@ -77,7 +77,6 @@ def _get_stack(nflxprofile_nodes, node_id, has_node_stack=False, pid_comm=None, 
     """Get node stack using parent pointers or predefined stack."""
     inverted = args.get("inverted", False)
     package_name = args.get("package_name", False)
-    ignore_libtype = args.get("ignore_libtype", False)
 
     stack = []
 
@@ -93,8 +92,7 @@ def _get_stack(nflxprofile_nodes, node_id, has_node_stack=False, pid_comm=None, 
         for name in function_name_arr:
             stack_frame = nflxprofile_pb2.StackFrame()
             stack_frame.function_name = name
-            if not ignore_libtype:
-                stack_frame.libtype = nflxprofile_nodes[node_id].libtype
+            stack_frame.libtype = nflxprofile_nodes[node_id].libtype
             stack.append(stack_frame)
         if inverted:
             return reversed(stack)
@@ -108,8 +106,7 @@ def _get_stack(nflxprofile_nodes, node_id, has_node_stack=False, pid_comm=None, 
             function_name = pid_comm[pid]
         stack_frame = nflxprofile_pb2.StackFrame()
         stack_frame.function_name = function_name
-        if not ignore_libtype:
-            stack_frame.libtype = nflxprofile_nodes[node_id].libtype
+        stack_frame.libtype = nflxprofile_nodes[node_id].libtype
         stack = [stack_frame] + list(nflxprofile_nodes[node_id].stack)
         if inverted:
             return reversed(stack)
@@ -119,14 +116,14 @@ def _get_stack(nflxprofile_nodes, node_id, has_node_stack=False, pid_comm=None, 
     nflxprofile_node_id = node_id
     while True:
         nflxprofile_node = nflxprofile_nodes[nflxprofile_node_id]
-        stack_frame = nflxprofile_pb2.StackFrame()
-        stack_frame.function_name = nflxprofile_node.function_name
-        if not ignore_libtype:
-            stack_frame.libtype = nflxprofile_node.libtype
         if inverted:
-            stack.append(stack_frame)
+            stack.append((nflxprofile_node.function_name, nflxprofile_node.libtype))
         else:
-            stack.insert(0, stack_frame)
+            stack_frame = nflxprofile_pb2.StackFrame()
+            stack_frame.function_name = nflxprofile_node.function_name
+            stack_frame.libtype = nflxprofile_node.libtype
+            stack.insert(
+                0, stack_frame)
         if not nflxprofile_nodes[nflxprofile_node_id].parent:
             break
         nflxprofile_node_id = nflxprofile_node.parent
