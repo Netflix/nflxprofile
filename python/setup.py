b74ac024d616cae735de79aa53e07c1512897759
Jason Koch <jkoch@netflix.com>
2022-03-16 11:19:33 -0700

    task: bump version

--- python/setup.py
+++ python/setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="nflxprofile",
-    version="1.6.4",
+    version="1.6.5",
     author="Mary Marchini",
     author_email="mmarchini@netflix.com",
     description="Protobuf specification of the nflxprofile format",
